cebe1d61cf9e82297dcb0c59cdc703b9cef36664
backy@ext-rlx-ub1 <backy@ext-rlx-ub1>
2025-06-04 10:20:26 +0200

    updated by bbh-update-github on 2025-06-04 10:20:26

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -1,5 +1,9 @@
 sudo apt install stow -y
 
+# Backup existing .bashrc and .bash_profile if they exist
+[ -f "$HOME/.bashrc" ] && mv "$HOME/.bashrc" "$HOME/.bashrc_BACK"
+[ -f "$HOME/.bash_profile" ] && mv "$HOME/.bash_profile" "$HOME/.bash_profile_BACK"
+
 git clone git@github.com:backy2222/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 stow bash
@@ -7,4 +11,3 @@ sudo stow -t / scripts
 echo "Dotfiles cloned. Bash & scripts deployed by stow."
 source ~/.bash_profile
 echo "Now run setup-all-machines.sh"
-
